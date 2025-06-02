d726f78212fee58ddef00667cd862d7c41d3555a
dvd20 <dvd2.0@live.com>
2025-06-02 10:57:09 -0300

    Good Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -31,6 +33,9 @@ public class Reservation {
 
 
     public Reservation(int roomNumber, Date checkin, Date checkout) {
+        if (!checkout.after(checkin)){
+            throw new DomainException("Error in reservation, Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkin = checkin;
         this.checkout = checkout;
@@ -41,21 +46,21 @@ public class Reservation {
          return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut){
+    public void updateDates(Date checkIn, Date checkOut){
 
         Date now = new Date();
 
-        if (checkIn.before(now) || checkOut.before(now)){
-             return "Error in reservation: Reservation dates for update must be future dates";
+        if (checkIn.before(now) || checkOut.before(now)) {
+             throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
         }
         if (!checkOut.after(checkIn)){
-            return "Error in reservation, Check-out date must be after check-in date";
+            throw new DomainException("Error in reservation, Check-out date must be after check-in date");
         }
 
 
         this.checkin = checkIn;
         this.checkout = checkOut;
-        return null;
+
     }
 
 
